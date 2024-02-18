255f990d814c1e1fb7e48c0c8ba06ce0f3b38bdf
Kirill-Yurkov <147937784+Kirill-Yurkov@users.noreply.github.com>
2024-02-18 12:15:50 +0300

    main pushed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
+import Emotions.EmotionType;
+import Locations.Home;
+import Locations.MedicineCenter;
+import Locations.Store;
+import TextObjects.*;
+import Weather.WeatherType;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person louis = new Person("Louis");
+        Person rachel = new Person("Rachel");
+        Person gage = new Person("Gage");
+        Store store = new Store("SuperMarket");
+        Home home = new Home("Louis' Home");
+        MedicineCenter medicineCenter = new MedicineCenter("City Medical Center");
+        Parrot parrot = new Parrot("Kesha");
+        WeatherType currentWeatherType;
+
+        home.addPeople(louis,rachel,gage);
+        home.startCinema();
+        louis.setCurrentEmotion(EmotionType.SURPRISE);
+        parrot.memorizePhrase("Ариэль Шэрон свихнулся");
+        parrot.say();
+        rachel.openBirdCage(parrot);
+        gage.chaseParrot(parrot);
+        louis.haveConversation(rachel, "telling funny joke");
+        louis.haveConversation(gage, "telling funny joke");
+        louis.checkHealth();
+        home.cookFood(FoodType.PASTA);
+        louis.eat(FoodType.PASTA);
+        louis.expressLoveFor(rachel);
+        louis.expressLoveFor(gage);
+        parrot.clearMemorize();
+        home.exitLocation(louis);
+
+        currentWeatherType = WeatherType.RAINY;
+
+        louis.setLocation(store);
+        store.interactInside(louis);
+        store.buyToy(louis);
+        store.buyCar(new Car("Toyota"), louis);
+        louis.eat(store.buyFastFood(FoodType.CHIPS, louis));
+        store.exitLocation(louis);
+
+        louis.getCar().start(CarSeat.DRIVER);
+        louis.driveCar(CarSeat.DRIVER, currentWeatherType);
+        louis.setCurrentEmotion(EmotionType.FEAR);
+        louis.getCar().stop();
+
+        louis.setLocation(medicineCenter);
+        medicineCenter.buyMedicine(louis);
+        medicineCenter.interactInside(louis);
+        medicineCenter.exitLocation(louis);
+
+        louis.setCurrentEmotion(EmotionType.SADNESS);
+        currentWeatherType = WeatherType.SUNNY;
+        louis.getCar().start(CarSeat.DRIVER);
+        louis.driveCar(CarSeat.DRIVER, currentWeatherType);
+        louis.playCarMusic();
+        louis.getCar().stop();
+        louis.stopCarMusic();
     }
 }
+
